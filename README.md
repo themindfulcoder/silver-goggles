e0ac7043bfe068ab18034ef89981c8ec6afe7d79
Dwayne Hinterlang <dwayne@themindfulcoder.com>
2023-11-25 00:57:39 +0100

    day 2

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # silver-goggles
+
+## requirements
+- docker
+
+## build
+docker build -t silver-goggles-image .
+
+## run
+docker run -dit --name silver-goggles-container -p 8080:80 silver-goggles-image
---
+++ src/weather-location-selector.js
@@ -0,0 +1,20 @@
+function saveWeatherLocationSelector(val) {
+  localStorage.setItem("silver-goggles-weatherlocation", val);
+}
+
+let weatherLocationSelectorElement = document.getElementById(
+  "weather-location-selector"
+);
+
+weatherLocationSelectorElement.onblur = (ev) => {
+  var value = ev.srcElement.value;
+  saveWeatherLocationSelector(value);
+};
+
+weatherLocationSelectorElement.onkeyup = (ev) => {
+  if (ev.keyCode != 13) {
+    return;
+  }
+  var value = ev.srcElement.value;
+  saveWeatherLocationSelector(value);
+};
